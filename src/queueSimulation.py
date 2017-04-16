042dc784de95dd55008837d9f97e6b7f48309ecb
timac11 <timac11@bk.ru>
2017-04-16 12:31:46 +0300

    implemented script of simple simulation

--- src/queueSimulation.py
+++ src/queueSimulation.py
@@ -1,17 +1,28 @@
 import random
-from network import Network as network
-from event import Event as event
-from packet import Packet as packet
-from station import Station as station
-import Generator.NormalRandomGenerator as generator
+
+class Station:
+    def __init__(self, t_gen):
+        self.packet_queue = []
+        self.t_gen = t_gen
+
+
+class Packet:
+    def __init__(self, size):
+        self.stage = 0
+        self.count = 0
+        self.transmitted_bytes = 0
+        self.size = size
+        self.statement = 'init'
+
 
 # global variables
-T_CURR_MAX = 1000
+T_CURR_MAX = 200
 event_queue = []
 time_gen = []
 
+# todo delete hardcoded t_gen
 # hardcoded for debug. None is queue of packets in stations in init
-stations = [station(1, 2, 1, None), station(2, 2, 1, None), station(3, 2, 1, None)]
+stations = [Station([10, 30, 100]), Station([60, 80])]
 
 
 def get_stations_ids():
@@ -21,21 +32,72 @@ def get_stations_ids():
     return stations_ids
 
 
-def change_state(station):
-    pass
+def change_state(station, env_statement):
+    if len(station.packet_queue) > 0:
+        # change state of the last packet on the queue of packet
+        # in the station
+        change_packet_state(station.packet_queue, env_statement)
+
+
+def choose_backoff():
+    # todo delete hardcode
+    return 10
+
+
+def change_packet_state(packets, env_statement):
+    for packet in packets:
+        if env_statement == 'free':
+            # check all possible statements of packet
+            if packet.statement == 'missing':
+                if packet.count == 0:
+                    packet.statement = 'transmit'
+                else:
+                    packet.count += 1
+            if packet.statement == 'transmit':
+                # hardcoded change of transmitted bytes
+                packet.transmited_size += 1
+                env_statement = 'busy'
+            if packet.statement == 'init':
+                packet.statement = 'missing'
+                packet.stage = choose_backoff()
+                packet.count = packet.stage
+            if packet.statement == 'sleep':
+                if packet.count == 0:
+                    packet.statement = 'transmit'
+                    packet.transmited_size += 1
+                else:
+                    packet.statement = 'missing'
+        else:
+            if station.statement == 'transmit':
+                # hardcoded
+                station.transmitted_size += 1
+                if packet.transmited_size == packet.size:
+                    # delete packet from queue of packets
+                    packets.remove(packet)
+            else:
+                station.statement = 'sleep'
+
+
+def generate_packet(station):
+    # hardcoded packet size
+    new_packet = Packet(10)
+    station.packet_queue.append(new_packet)
 
 
 def simulation():
 
-    # implement data for generation
-    stations_ids = get_stations_ids()
-    time_gen = generator.init(stations_ids, T_CURR_MAX)
-
+    env_statement = 'free'
     # life cycle of the network
     time_current = 0
     while time_current <= T_CURR_MAX:
-        time_current += 1
         for station in stations:
-            change_state(station)
+            change_state(station, env_statement)
+            # check if not empty
+            if len(station.t_gen) > 0:
+                if time_current == station.t_gen[-1]:
+                    generate_packet(station)
+                    station.t_gen.remove(time_current)
+        time_current += 1
 
-
+###################################################################
+simulation()
